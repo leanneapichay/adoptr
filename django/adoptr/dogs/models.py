8fc975ab6a4ae2599f2e2bfacd8bcd1ebe1bd4e2
andrewsong24 <andrewsong24@gmail.com>
2018-04-28 21:46:13 -0400

    added model for dog

--- django/adoptr/dogs/models.py
+++ django/adoptr/dogs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Dog(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.SmallIntegerField()
+    breed = models.CharField(max_length=100)
+    description = models.TextField()
+
