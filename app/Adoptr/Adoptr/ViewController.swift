0d8c8adbe684d3dba068b46743375c2b4390d902
Leanne Pichay <leannepichay@Leannes-Air-2.T-mobile.com>
2018-05-17 21:15:14 -0700

    app

---
+++ app/Adoptr/Adoptr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Adoptr
+//
+//  Created by Leanne Pichay on 17/5/18.
+//  Copyright © 2018 adoptr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
