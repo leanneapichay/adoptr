0945f003e4f8f183dc6e72c7115cb70b8ae4ef5e
Leanne Pichay <leannepichay@leannes-macbook-air-2.local>
2018-05-18 13:26:47 -0700

    '

--- app/Adoptr/Adoptr/PageViewController.swift
+++ app/Adoptr/Adoptr/PageViewController.swift
@@ -9,30 +9,63 @@
 import UIKit
 
 class PageViewController: UIPageViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                                animated: true,
+                                completion: nil)
+        }
+        
     }
-    
 }
     
-
 extension PageViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        return nil
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        return nil
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
     }
 }
 
 private(set) var orderedViewControllers : [UIViewController] = {
     return  [newViewController(name: "OpeningScreen"),
-             newViewController(name: "SignIn")
-    ]
+             newViewController(name: "SignIn")]
 }()
 
 private func newViewController(name: String) -> UIViewController {
