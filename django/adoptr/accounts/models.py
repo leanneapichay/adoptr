8a3380e793ddd0ec9eb4b30108b011f43c3e06c7
andrewsong24 <andrewsong24@gmail.com>
2018-05-28 23:30:29 -0400

    custom user

--- django/adoptr/accounts/models.py
+++ django/adoptr/accounts/models.py
@@ -1,16 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import User
 from shelters.models import Shelter
+from django.contrib.auth.models import AbstractBaseUser
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+
+    zip_code = models.SmallIntegerField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Adopter(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    zip_code = models.SmallIntegerField()
     availability = models.SmallIntegerField()
-    size = models.IntegerField()
-    age = models.IntegerField()
-    traits = models.IntegerField()
+    size = models.CharField(max_length=3)
+    age = models.CharField(max_length=3)
+    traits = models.CharField(max_length=3)
 
     def __str__(self):
         return self.user.email
@@ -19,7 +31,6 @@ class Adopter(models.Model):
 class Giver(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    zip_code = models.SmallIntegerField()
     independent = models.BooleanField()
     shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE, null=True, blank=True)
 
