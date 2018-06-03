4eae89a2d3a13667ef01ebe7c5149479ae744b30
andrewsong24 <andrewsong24@gmail.com>
2018-06-03 00:11:53 -0400

    fixed feed algorithm

--- django/adoptr/feed/feed_algorithm.py
+++ django/adoptr/feed/feed_algorithm.py
@@ -9,57 +9,33 @@ def get_query(char_list):
     q_objects.add(Q(active=char_list[1]), Q.AND)
     q_objects.add(Q(trained=char_list[2]), Q.AND)
 
-    num = 0
+    size_query = Q()
 
     for i in range(3, 6):
         if char_list[i] is True:
-            num += 1
+            if i == 3:
+                size_query.add(Q(size=0), Q.OR)
+            elif i == 4:
+                size_query.add(Q(size=1), Q.OR)
+            elif i == 5:
+                size_query.add(Q(size=2), Q.OR)
 
-    if num != 0:
-        for i in range(3, 6):
-            if char_list[i] is True and num > 1:
-                if i == 3:
-                    q_objects.add(Q(size=0), Q.OR)
-                elif i == 4:
-                    q_objects.add(Q(size=1), Q.OR)
-                elif i == 5:
-                    q_objects.add(Q(size=2), Q.OR)
+    q_objects.add(size_query, Q.AND)
 
-                num -= 1
-
-            elif char_list[i] is True and num == 1:
-                if i == 3:
-                    q_objects.add(Q(size=0), Q.AND)
-                elif i == 4:
-                    q_objects.add(Q(size=1), Q.AND)
-                elif i == 5:
-                    q_objects.add(Q(size=2), Q.AND)
-
-    num = 0
+    age_query = Q()
 
     for i in range(6, 9):
         if char_list[i] is True:
-            num += 1
+            if i == 6:
+                age_query.add(Q(age_range=0), Q.OR)
+            if i == 7:
+                age_query.add(Q(age_range=1), Q.OR)
+            if i == 8:
+                age_query.add(Q(age_range=2), Q.OR)
 
-    if num != 0:
-        for i in range(6, 9):
-            if char_list[i] is True and num > 1:
-                if i == 6:
-                    q_objects.add(Q(age=0), Q.OR)
-                if i == 7:
-                    q_objects.add(Q(age=1), Q.OR)
-                if i == 8:
-                    q_objects.add(Q(age=2), Q.OR)
-            elif char_list[i] is True and num == 1:
-                if i == 6:
-                    q_objects.add(Q(age=0), Q.AND)
-                if i == 7:
-                    q_objects.add(Q(age=1), Q.AND)
-                if i == 8:
-                    q_objects.add(Q(age=2), Q.AND)
+    q_objects.add(age_query, Q.AND)
 
     return q_objects
-
 
 
 
