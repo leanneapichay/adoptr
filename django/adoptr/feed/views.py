b0b7c7bd17f59955de2d80c37079d35a6021ffed
andrewsong24 <andrewsong24@gmail.com>
2018-06-03 16:22:29 -0400

    generalized get matches and complete matches

--- django/adoptr/feed/views.py
+++ django/adoptr/feed/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from accounts.models import Adopter, User
+from accounts.models import Adopter, User, Giver
 from .models import MatchPP, MatchSP
 from .serializers import MatchPPSerializer, MatchSPSerializer
 from dogs.models import Dog
@@ -77,54 +77,71 @@ def create_match_pp(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# for adopters and givers, not shelters
 @api_view(['PUT'])
 def get_non_completed_matches(request):
 
-    adopter_email = request.data.get('email')
+    email = request.data.get('email')
 
     try:
-        user = User.objects.get(email=adopter_email)
+        user = User.objects.get(email=email)
     except User.DoesNotExist:
         return Response('User Not Found', status=status.HTTP_404_NOT_FOUND)
 
     try:
         adopter = Adopter.objects.get(user_id=user.id)
+        is_adopter = True
     except Adopter.DoesNotExist:
-        return Response('Adopter Not Found', status=status.HTTP_404_NOT_FOUND)
+        try:
+            giver = Giver.objects.get(user_id=user.id)
+            is_adopter = False
+        except Giver.DoesNotExist:
+            return Response('User Not Found', status=status.HTTP_404_NOT_FOUND)
 
-    matches_pp = MatchPP.objects.filter(adopter_id=adopter.id, complete=False)
-    matches_sp = MatchSP.objects.filter(adopter_id=adopter.id, complete=False)
-
-    match_pp_serializer = MatchPPSerializer(matches_pp, many=True)
-    match_sp_serializer = MatchSPSerializer(matches_sp, many=True)
-
-    return Response({'People': match_pp_serializer.data, 'Shelters': match_sp_serializer.data})
+    if is_adopter is True:
+        matches_pp = MatchPP.objects.filter(adopter_id=adopter.id, complete=False)
+        matches_sp = MatchSP.objects.filter(adopter_id=adopter.id, complete=False)
+        match_pp_serializer = MatchPPSerializer(matches_pp, many=True)
+        match_sp_serializer = MatchSPSerializer(matches_sp, many=True)
+        return Response({'People': match_pp_serializer.data, 'Shelters': match_sp_serializer.data},
+                        status=status.HTTP_200_OK)
+    else:
+        match_pp = MatchPP.objects.filter(owner_id=giver.id, complete=False)
+        match_pp_serializer = MatchPPSerializer(match_pp, many=True)
+        return Response(match_pp_serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['PUT'])
 def get_completed_matches(request):
 
-    adopter_email = request.data.get('email')
+    email = request.data.get('email')
 
     try:
-        user = User.objects.get(email=adopter_email)
+        user = User.objects.get(email=email)
     except User.DoesNotExist:
         return Response('User Not Found', status=status.HTTP_404_NOT_FOUND)
 
     try:
         adopter = Adopter.objects.get(user_id=user.id)
+        is_adopter = True
     except Adopter.DoesNotExist:
-        return Response('Adopter Not Found', status=status.HTTP_404_NOT_FOUND)
+        try:
+            giver = Giver.objects.get(user_id=user.id)
+            is_adopter = False
+        except Giver.DoesNotExist:
+            return Response('User Not Found', status=status.HTTP_404_NOT_FOUND)
 
-    accepted_deals_pp = MatchPP.objects.filter(adopter_id=adopter.id, complete=True)
-
-    accepted_deals_sp = MatchSP.objects.filter(adopter_id=adopter.id, complete=True)
-
-    match_pp_serializer = MatchPPSerializer(accepted_deals_pp, many=True)
-    match_sp_serializer = MatchSPSerializer(accepted_deals_sp, many=True)
-
-    return Response({'People': match_pp_serializer.data, 'Shelters': match_sp_serializer.data},
-                    status=status.HTTP_200_OK)
+    if is_adopter is True:
+        matches_pp = MatchPP.objects.filter(adopter_id=adopter.id, complete=True)
+        matches_sp = MatchSP.objects.filter(adopter_id=adopter.id, complete=True)
+        match_pp_serializer = MatchPPSerializer(matches_pp, many=True)
+        match_sp_serializer = MatchSPSerializer(matches_sp, many=True)
+        return Response({'People': match_pp_serializer.data, 'Shelters': match_sp_serializer.data},
+                        status=status.HTTP_200_OK)
+    else:
+        match_pp = MatchPP.objects.filter(owner_id=giver.id, complete=True)
+        match_pp_serializer = MatchPPSerializer(match_pp, many=True)
+        return Response(match_pp_serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['PUT'])
